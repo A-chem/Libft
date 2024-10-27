6be09e44fd2936b13b282c5fcbc213ac01c1041b
Abdelilah Chemlal <achemlal@e2r10p15.1337.ma>
2024-10-27 12:34:04 +0100

    first calloc

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achemlal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 11:57:41 by achemlal          #+#    #+#             */
+/*   Updated: 2024/10/27 12:31:15 by achemlal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_calloc(size_t count, size_t size)
+{
+	unsigned char *p;
+
+	p = malloc(count * size);
+	if (!p)
+		return (NULL);
+	ft_bzero(p, count * size);
+	return (p);
+}
